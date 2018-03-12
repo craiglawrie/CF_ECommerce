4b8c555dba74d066d2d6511d8997e7a91e550bc9
Craig Lawrie <34558661+craiglawrie@users.noreply.github.com>
2018-03-12 10:05:00 -0400

    initial commit

---
+++ CF_ECommerce/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CF_ECommerce.Startup))]
+namespace CF_ECommerce
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
